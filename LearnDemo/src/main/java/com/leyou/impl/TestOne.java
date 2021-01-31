ec402050468672268a4560f7de515eee46a1afdb
liuweisheng <liuweisheng8149199@163.com>
2021-01-31 21:17:37 +0800

    第三次提交

--- LearnDemo/src/main/java/com/leyou/impl/TestOne.java
+++ LearnDemo/src/main/java/com/leyou/impl/TestOne.java
@@ -3,6 +3,7 @@ package com.leyou.impl;
 public class TestOne {
     public static void main(String[] args) {
         String str =  "这是我的第er个测试分支";
-        System.out.println(str);
+        String string ="这是什么玩意";
+        System.out.println(str+string);
     }
 }
