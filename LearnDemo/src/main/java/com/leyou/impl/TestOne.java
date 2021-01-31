7c2204cb3b6501f5927e4ecca9ec54e6c59ef5c0
liuweisheng <liuweisheng8149199@163.com>
2021-01-31 18:26:47 +0800

    提交修改的测试分支

--- LearnDemo/src/main/java/com/leyou/impl/TestOne.java
+++ LearnDemo/src/main/java/com/leyou/impl/TestOne.java
@@ -2,7 +2,7 @@ package com.leyou.impl;
 
 public class TestOne {
     public static void main(String[] args) {
-        String str =  "这是我的第一个测试分支";
+        String str =  "这是我的第er个测试分支";
         System.out.println(str);
     }
 }
